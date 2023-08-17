630cae745fdb804f010917f6cf67cbb36b40d61c
dongwei <jackblack369@163.com>
2023-08-18 02:53:25 +0800

    init code

---
+++ src/main/java/org/apache/flink/streaming/connectors/redis/common/util/JarUtil.java
@@ -0,0 +1,14 @@
+/*
+package org.apache.flink.streaming.connectors.redis.common.util;
+
+import io.micrometer.core.instrument.TimeGauge;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
+import java.util.concurrent.TimeUnit;
+
+public interface JarUtil {
+
+    TimeGauge gauge = TimeGauge.builder("time.gauge", null, TimeUnit.MILLISECONDS, o -> 1.0)
+            .register(new SimpleMeterRegistry());
+}
+*/
